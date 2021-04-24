3d0376998d2b0020f3e1be2c31087557630e3940
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2021-04-24 18:13:31 +0100

    Note class documentation created

--- src/note.ts
+++ src/note.ts
@@ -1,11 +1,28 @@
 import * as fs from 'fs';
 import * as chalk from 'chalk';
 
+/**
+ * Type representing the colors accepted by the system.
+ */
 export type TypeColor = 'red' | 'green' |'blue' | 'yellow';
 
+/**
+ * Class that allows adding, modifying, deleting, listing and reading notes of a specific user.
+ */
 export class Note {
+  /**
+   * Class constructor.
+   */
   constructor() {}
 
+  /**
+   * Method that allows adding a note to the list.
+   * @param userName The username to add the note.
+   * @param title The title of the note.
+   * @param message The message that the note contains.
+   * @param color The color of the note.
+   * @returns A message indicating if the note has been created correctly or if another with the same title already exists.
+   */
   addNote(userName: string, title: string, message: string, color: TypeColor): string {
     if (fs.existsSync(`notes/${userName}/${title}.json`)) {
       console.log(chalk.bold.red('Note title taken!'));
@@ -18,6 +35,14 @@ export class Note {
     return 'New note added!';
   }
 
+  /**
+   * Method that allows modifying a note in the list.
+   * @param userName The username to modify the note.
+   * @param title The title of the note.
+   * @param message The message that the note contains.
+   * @param color The color of the note.
+   * @returns A message indicating if the note has been modified correctly or if the note does not exist.
+   */
   modifyNote(userName: string, title: string, message: string, color: TypeColor): string {
     if (!fs.existsSync(`notes/${userName}/${title}.json`)) {
       console.log(chalk.bold.red('The note you want to modify does not exist!'));
@@ -29,6 +54,12 @@ export class Note {
     return 'Note modified!';
   }
 
+  /**
+   * Method to remove a note from the list.
+   * @param userName The username to remove the note.
+   * @param title The title of the note.
+   * @returns A message indicating if the note was not found or if it was successfully deleted.
+   */
   removeNote(userName: string, title: string): string {
     if (!fs.existsSync(`notes/${userName}/${title}.json`)) {
       console.log(chalk.bold.red('Note not found'));
@@ -39,6 +70,11 @@ export class Note {
     return 'Note removed!';
   }
 
+  /**
+   * Method that lists the titles of all a user's notes.
+   * @param userName The username to show the notes.
+   * @returns A message with the titles of the notes or an error if the user does not have any saved notes.
+   */
   showNotes(userName: string): string {
     if (!fs.existsSync(`notes/${userName}`)) {
       console.log(chalk.bold.red('You have never saved a note'));
@@ -56,6 +92,12 @@ export class Note {
     return textNotes;
   }
 
+  /**
+   * Method that allows you to read a specific note from the list.
+   * @param userName The username to read a note.
+   * @param title The title of the note.
+   * @returns A message stating that the note was not found or the titles and content of each note.
+   */
   readNote(userName: string, title: string): string {
     if (!fs.existsSync(`notes/${userName}/${title}.json`)) {
       console.log(chalk.bold.red('Note not found'));
