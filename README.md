7a3c2b943b5edb8e7da1b04887dc9fd864da6702
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2021-04-24 17:43:08 +0100

    Failed Note class tests

--- README.md
+++ README.md
@@ -19,3 +19,5 @@
 En esta práctica se implementa una aplicación de procesamiento de notas de texto. En concreto, permite añadir, modificar, eliminar, listar y leer notas de un usuario concreto. Las notas se almacenan como ficheros JSON en el sistema de ficheros de la máquina que ejecute la aplicación. Sólo se puede interactuar con la aplicación desde la línea de comandos.
 
 Además, se llevará a cabo un informe donde se muestran y explican las soluciones a los ejercicios.
+
+Los ejercicios realizados durante el PE-103 se encuentran en los directorios src/ejercicios-PE103 y tests/ejercicios-PE103.
---
+++ tests/note.spec.ts
@@ -0,0 +1,61 @@
+import 'mocha';
+import {expect} from 'chai';
+import * as fs from 'fs';
+import {Note} from '../src/notes';
+
+
+describe('Note class tests', () => {
+  const note = new Note();
+
+  it('A Note class object can be successfully created', () => {
+    expect(note).not.to.be.equal(null);
+  });
+
+  it('note.addNode("test", "Test note", "This is a test note", "green") returns "New note added!"', () => {
+    expect(note.addNote('test', 'Test note', 'This is a test note', 'green')).to.be.equal('New note added!');
+  });
+
+  it('The note was created successfully', () => {
+    expect(fs.existsSync(`notes/test/Test note.json`)).to.be.equal(true);
+  });
+
+  it('note.addNode("test", "Test note", "This is a test note", "green") returns "Note title taken!"', () => {
+    expect(note.addNote('test', 'Test note', 'This is a test note', 'green')).to.be.equal('Note title taken!');
+  });
+
+  it('note.modifyNote("test", "Test note", "Testing the modify method", "blue") returns "Note modified!"', () => {
+    expect(note.modifyNote('test', 'Test note', 'Testing the modify method', 'blue')).to.be.equal('Note modified!');
+  });
+
+  it('The file has been modified successfully', () => {
+    expect(fs.readFileSync(`notes/test/Test note.json`, {encoding: 'utf-8'})).to.be.equal('{"title": "Test note", "message": "Testing the modify method", "color": "blue"}');
+  });
+
+  it('note.modifyNote("test", "Non-existent file", "Testing the modify method", "blue") returns "The note you want to modify does not exist!"', () => {
+    expect(note.modifyNote('test', 'Non-existent file', 'Testing the modify method', 'blue')).to.be.equal('The note you want to modify does not exist!');
+  });
+
+  it('note.showNotes("test") returns "Test note "', () => {
+    expect(note.showNotes('test')).to.be.equal('Test note ');
+  });
+
+  it('note.showNotes("Non-existent user") returns "You have never saved a note"', () => {
+    expect(note.showNotes('Non-existent user')).to.be.equal('You have never saved a note');
+  });
+
+  it('note.readNote("test", "Test note") returns "Test note Testing the modify method"', () => {
+    expect(note.readNote('test', 'Test note')).to.be.equal('Test note\nTesting the modify method');
+  });
+
+  it('note.readNote("test", "Non-existent file") returns "Note not found"', () => {
+    expect(note.readNote('test', 'Non-existent file')).to.be.equal('Note not found');
+  });
+
+  it('note.removeNote("test", "Test note") returns "Note removed!"', () => {
+    expect(note.removeNote('test', 'Test note')).to.be.equal('Note removed!');
+  });
+
+  it('The note has been deleted successfully', () => {
+    expect(note.removeNote('test', 'Test note')).to.be.equal('Note not found');
+  });
+});
